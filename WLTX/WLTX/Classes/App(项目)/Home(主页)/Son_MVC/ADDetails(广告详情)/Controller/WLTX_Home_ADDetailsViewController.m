9dcab08983927810e4087c8c451a461429a7cf81
lyh2019 <liyuhong2019@163.com>
2019-07-18 08:27:17 +0800

    20190718 0828 修改广告页面信息
    20190718 0828 修改广告页面信息

--- WLTX/WLTX/Classes/App(项目)/Home(主页)/Son_MVC/ADDetails(广告详情)/Controller/WLTX_Home_ADDetailsViewController.m
+++ WLTX/WLTX/Classes/App(项目)/Home(主页)/Son_MVC/ADDetails(广告详情)/Controller/WLTX_Home_ADDetailsViewController.m
@@ -1,10 +1,14 @@
-//
-//  WLTX_Home_ADDetailsViewController.m
-//  WLTX
-//
-//  Created by liyuhong2019 on 2019/4/12.
-//  Copyright © 2019 liyuhong165. All rights reserved.
-//
+/**
+ 20190717  更新
+ 1、识别页面的内容          https://www.jianshu.com/p/b4f2951e07c4
+ 2、获取页面的电话          https://www.cnblogs.com/adampei-bobo/p/8375931.html
+ 3、删除页面的电话下划线     https://www.jianshu.com/p/b44cfcc3d851
+ 4、webview 自适应屏幕    https://blog.csdn.net/qq_31258413/article/details/82018301
+ 5、webview 禁止长按     https://www.jianshu.com/p/eb63e93d04e6
+ 参考
+ https://www.jianshu.com/p/6bbcc438b188
+ 
+ */
 
 #import "WLTX_Home_ADDetailsViewController.h"
 
@@ -14,10 +18,18 @@ UIWebViewDelegate
 >
 @property (weak, nonatomic) IBOutlet UIWebView *webview;
 
+@property (nonatomic,strong) NSMutableArray *allPhoneNumber;
 @end
 
 @implementation WLTX_Home_ADDetailsViewController
 
+- (NSMutableArray *)allPhoneNumber
+{
+    if (_allPhoneNumber == nil) {
+        _allPhoneNumber = [NSMutableArray array];
+    }
+    return _allPhoneNumber;
+}
 #pragma mark - ♻️ 视图的生命周期 view life cycle start
 /*
  4-1、第一个执行的方法，加载UI：- (void)loadView{ }
@@ -44,8 +56,8 @@ UIWebViewDelegate
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [self aboutUsVC_netwrok_getADdetailRequest];
-//    [self loadWebData];
+//    [self aboutUsVC_netwrok_getADdetailRequest];
+    [self loadWebData];
     
 }
 - (void)viewDidAppear:(BOOL)animated
@@ -96,16 +108,105 @@ UIWebViewDelegate
 //
 //
 //}
+
+/**加载页面的数据
+ 
+ */
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+    NSLog(@"网页加载完成才能获取");
+    
+    NSString *lJs2 = @"document.documentElement.innerText"; //根据标识符获取不同内容
+    NSString *lHtml2 = [webView stringByEvaluatingJavaScriptFromString:lJs2];
+    NSLog(@"网页内容为: %@",lHtml2);
+    NSString *string  = lHtml2;
+    NSString *pattern;
+
+    
+    
+//    pattern=@"0\\d{2,3}[- ]\\d{7,8}";
+
+//    pattern=@"\\d{3,4}[- ]?\\d{7,8}";
+
+    
+    pattern=@"\\d{3,4}[- ]?\\d{7,8}";
+    
+//    string=@"2017-02-12 上报人:张三 15930384756";
+    NSError *error;
+    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+    
+    NSLog(@"%@",error.userInfo);
+    
+    [regex enumerateMatchesInString:string options:NSMatchingReportProgress range:NSMakeRange(0, string.length) usingBlock:^(NSTextCheckingResult * _Nullable result, NSMatchingFlags flags, BOOL * _Nonnull stop) {
+        if (NSMatchingReportProgress==flags) {
+//            NSLog(@"result %@",result);
+        }else{
+            /**
+             *  系统内置方法
+             */
+//            NSLog(@"result %@",result);
+
+            if (NSTextCheckingTypePhoneNumber==result.resultType) {
+                NSLog(@"%@",[string substringWithRange:result.range]);
+            }
+            /**
+             *  长度为11位的数字串、12是固定电话 xxx - xxxxxx
+             */
+            if (result.range.length==11 || result.range.length == 12) {
+                NSLog(@"电话有 : %@",[string substringWithRange:result.range]);
+                NSString *phoneNumber = [string substringWithRange:result.range];
+                [self.allPhoneNumber addObject:phoneNumber];
+            }
+            
+        }
+    }];
+    NSLog(@"所有的手机号码 %@",self.allPhoneNumber);
+    
+// 禁止长按
+    [self.webview stringByEvaluatingJavaScriptFromString:(@"document.documentElement.style.webkitUserSelect='none';")];
+    [self.webview stringByEvaluatingJavaScriptFromString:(@"document.documentElement.style.webkitTouchCallout='none';")];
+
+
+    
+// 
+    NSString *js = [NSString stringWithFormat:@"function autoFit() { \
+                                          var imgs = document.getElementsByTagName('img'); \
+                                          for (var i = 0; i < imgs.length; ++i) {\
+                                          var img = imgs[i];   \
+                                          img.style.width = %f ;   \
+                                          img.style.height = 'auto';   \
+                                          } \
+                                          }",[UIScreen mainScreen].bounds.size.width-20];
+    [self.webview stringByEvaluatingJavaScriptFromString:js];
+    [self.webview stringByEvaluatingJavaScriptFromString:@"autoFit()"];
+  
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    
+    return nil;
+    
+}
+
 #pragma mark 🏃(代理)Delegate end
 #pragma mark - ✍🏻(自定义方法) custom method start
+/**
+ 获取页面信息 https://www.jianshu.com/p/b4f2951e07c4
+ 
+ */
 - (void)loadWebData
 {
-    NSURL *url = [NSURL URLWithString:self.weburl];
     
+    NSString * url = [NSString stringWithFormat:@"%@%@%@",WLTX_DomainOrIpUrl,Home_AdDetail,self.adId];
+    NSURL *weburl = [NSURL URLWithString:url];
     // 2. 把URL告诉给服务器,请求,从m.baidu.com请求数据
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLRequest *request = [NSURLRequest requestWithURL:weburl];
     // 3. 发送请求给服务器
     [self.webview loadRequest:request];
+    self.webview.dataDetectorTypes = UIDataDetectorTypeNone; // 防止固定电话默认样式
+
+    
+    
+
 
 }
 
@@ -127,7 +228,33 @@ UIWebViewDelegate
     self.navigationItem.title = @"物流天地";
     self.view.backgroundColor = UIColorFromRGB(0xF5F5F5);
     
+    [self adDetails_BarbuttonItem];
 }
+
+- (void)adDetails_BarbuttonItem
+{
+//    self.view.backgroundColor = UIColorFromRGB(0x000000);
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [backButton setTitle:@"联系我们" forState:0];
+    [backButton setTitleColor:[UIColor whiteColor] forState:0];
+    backButton.titleLabel.font = [UIFont systemFontOfSize:13];
+
+    backButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+    [backButton addTarget:self action:@selector(adContactUs:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+}
+
+/**参考
+ https://www.jianshu.com/p/63737bb81d82
+ */
+- (void)adContactUs:(UIButton *)btn
+{
+    NSLog(@"联系我们");
+    
+    NSLog(@"联系我们 %@",self.allPhoneNumber);
+
+}
+
 #pragma mark  ✍🏻(自定义方法) custom method end
 
 
--- WLTX/WLTX/Classes/OpenThreeSDKFoundation(开源第三方框架)/Tool/AFNetworkingTool/Tool/AFNetworkingTool.h
+++ WLTX/WLTX/Classes/OpenThreeSDKFoundation(开源第三方框架)/Tool/AFNetworkingTool/Tool/AFNetworkingTool.h
@@ -32,7 +32,9 @@
 #define ImgBaseURL          @"http://www.0201566.com"
 
 #else // 发布状态
+#define NotApiBaseURL    @"http://m.0201566.com/"
 #define BaseURL          @"http://m.0201566.com/appapi/"
+#define ImgBaseURL          @"http://www.0201566.com"
 #endif
 
 
